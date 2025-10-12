76be33be136aadf705e1224218697b974a602f73
Maman Dauletov <mamandauletov@192.168.1.3>
2025-10-12 19:03:29 +0500

    finish

--- english/models.py
+++ english/models.py
@@ -173,7 +173,6 @@ class Topic(models.Model):
         verbose_name='Глава'
     )
     name = models.CharField(max_length=255, verbose_name='Название темы')
-    video_path = models.CharField(max_length=255, verbose_name='Путь к видео')
     content = RichTextUploadingField(verbose_name="Содержание")
 
     class Meta:
