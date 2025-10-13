2efd8a6799a1d9aa79ae47db8320d8447bf549c8
Maman Dauletov <mamandauletov@MacBook-Air-Maman.local>
2025-10-13 16:44:58 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -125,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
