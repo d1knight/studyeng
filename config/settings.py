860aa15bd66026bf4d24bb6cf43f1f86fa3edd10
Maman Dauletov <mamandauletov@192.168.1.3>
2025-10-14 18:49:25 +0500

    test5

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,14 @@ CKEDITOR_CONFIGS = {
 }
 
 LOGIN_URL = '/accounts/login/'  # Соответствует пути к reg
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
