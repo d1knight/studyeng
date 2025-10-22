6339f29f7d67274436b8a513add3e48d5ce893e0
Your Name <you@example.com>
2025-10-22 14:35:56 +0500

    test14

--- config/settings.py
+++ config/settings.py
@@ -154,24 +154,3 @@ CACHES = {
     }
 }
 
-
-
-import logging
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/home/studyeng/django_debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
